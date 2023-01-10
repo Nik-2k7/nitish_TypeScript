a6b8ffc0e33f93c733f2ac1692f90974db64ea6c
Nitish Kumar <nitishk@crowdstaffing.com>
2023-01-10 14:48:22 +0530

    performed arithmetic operation on two numbers  and practiced let & var declaration

--- app.js
+++ app.js
@@ -23,3 +23,71 @@ var Car = /** @class */ (function () {
 }());
 var newCar = new Car('Lamborghini', 2, true, 'ZedBlack', 'Petrol', 6498);
 newCar.make();
+// Arithmetic operation on two numbers
+// addition
+function addNumbers(a, b) {
+    return a + b;
+}
+var sum = addNumbers(10, 15);
+console.log('Sum of the two numbers is: ' + sum);
+// Subtraction
+function subtractNumbers(a, b) {
+    return a - b;
+}
+var Difference = subtractNumbers(10, 15);
+console.log('Difference of the two numbers is: ' + Difference);
+// Multiplication
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+var sum = multiplyNumbers(10, 15);
+console.log('Multiplication of the two numbers is: ' + sum);
+// Divison
+function divideNumbers(a, b) {
+    return a / b;
+}
+var sum = divideNumbers(10, 15);
+console.log('Division of the two numbers is: ' + sum);
+// Type Annotations in TypeScript
+var age = 32; // number variable
+var tName = "John"; // string variable
+var isUpdated = true; // Boolean variable
+function display(id, tName) {
+    console.log("My name is ".concat(tName, ". My Age is ").concat(id));
+}
+display(32, "John");
+// let num1:number = 1; 
+// function letDeclaration() { 
+//     let num2:number = 2; 
+//     if (num2 > num1) { 
+//         let num3: number = 3;
+//         num3++; 
+//     } 
+//     while(num1 < num2) { 
+//         let num4: number = 4;
+//         num1++;
+//     }
+//     console.log(num1); //OK
+//     console.log(num2); //OK 
+//     // console.log(num3); //Compiler Error: Cannot find name 'num3'
+//     // console.log(num4); //Compiler Error: Cannot find name 'num4'
+// }
+// letDeclaration();
+var numb1 = 1;
+function varDeclaration() {
+    var num2 = 2;
+    var num3;
+    if (num2 > numb1) {
+        num3 = 3;
+        num3++;
+    }
+    while (numb1 < num2) {
+        var num4 = 4;
+        numb1++;
+    }
+    console.log(numb1); //2
+    console.log(num2); //2 
+    console.log(num3); //4
+    console.log(num4); //4
+}
+varDeclaration();
--- app.ts
+++ app.ts
@@ -33,3 +33,93 @@ class Car {
 }
 let newCar = new Car('Lamborghini', 2, true, 'ZedBlack', 'Petrol', 6498);
 newCar.make();
+
+   
+// Arithmetic operation on two numbers
+// addition
+function addNumbers(a: number, b: number) { 
+    return a + b; 
+} 
+
+var sum: number = addNumbers(10, 15) 
+console.log('Sum of the two numbers is: ' +sum); 
+// Subtraction
+function subtractNumbers(a: number, b: number) { 
+    return a - b; 
+} 
+
+var Difference: number = subtractNumbers(10, 15) 
+
+console.log('Difference of the two numbers is: ' +Difference); 
+// Multiplication
+function multiplyNumbers(a: number, b: number) { 
+    return a * b; 
+} 
+
+var sum: number = multiplyNumbers(10, 15) 
+
+console.log('Multiplication of the two numbers is: ' +sum); 
+// Divison
+function divideNumbers(a: number, b: number) { 
+    return a / b; 
+} 
+
+var sum: number = divideNumbers(10, 15) 
+
+console.log('Division of the two numbers is: ' +sum); 
+
+// Type Annotations in TypeScript
+var age: number = 32; // number variable
+var tName: string = "John";// string variable
+var isUpdated: boolean = true;// Boolean variable
+function display(id:number, tName:string)
+{
+    console.log(`My name is ${tName}. My Age is ${id}`)
+}
+display(32, "John");
+
+// letDeclaration();
+// let num1:number = 1; 
+    
+// function letDeclaration() { 
+//     let num2:number = 2; 
+
+//     if (num2 > num1) { 
+//         let num3: number = 3;
+//         num3++; 
+//     } 
+
+//     while(num1 < num2) { 
+//         let num4: number = 4;
+//         num1++;
+//     }
+
+//     console.log(num1); //OK
+//     console.log(num2); //OK 
+//     // console.log(num3); //Compiler Error: Cannot find name 'num3'
+//     // console.log(num4); //Compiler Error: Cannot find name 'num4'
+// }
+
+// var declaration
+var numb1:number = 1; 
+    
+function varDeclaration() { 
+    var num2:number = 2; 
+    var num3: number;
+    if (num2 > numb1) { 
+        num3 = 3;
+        num3++; 
+    } 
+
+    while(numb1 < num2) { 
+        var num4: number = 4;
+        numb1++;
+    }
+
+    console.log(numb1); //2
+    console.log(num2); //2 
+    console.log(num3); //4
+    console.log(num4); //4
+}
+
+varDeclaration();
